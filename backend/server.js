b7aa812647e7f86ad5e21a9b802b467396321039
Saurabh Kumar <44322770+ksaurabh252@users.noreply.github.com>
2026-06-11 23:21:33 +0530

    feat: configure database connection and API routes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dbConnect = require("./config/dbConfig");
 require("dotenv").config();
-const app = express();
 
+const userRoutes = require("./routes/user.routes");
+const taskRoutes = require("./routes/task.routes");
+
+const app = express();
 const PORT = process.env.PORT || 3000;
+
+//Database connection
+dbConnect();
+
 app.use(express.json());
+
+//Routes
+app.use("/api/users", userRoutes);
+app.use("/api/tasks", taskRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
