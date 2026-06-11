d1891a2f2204c651409f3e677c28f4476b99b871
Saurabh Kumar <44322770+ksaurabh252@users.noreply.github.com>
2026-06-11 23:18:02 +0530

    fix: update database URI environment key

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 // Connect to MongoDB
 const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
 
     console.log("MongoDB connected successfully");
   } catch (error) {
